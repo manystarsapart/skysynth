675c2e443155f7ecd53d44022a64a473a15094f1
manystarsapart <manystarsapart@gmail.com>
2025-05-10 11:44:21 +0800

    chore: changelog, vers no., dates

--- src/core/states.ts
+++ src/core/states.ts
@@ -2,8 +2,8 @@ import { keyboardMode0 } from "./maps";
 import { instruments, effectNodes, instrumentNames } from "../audio/instruEffect";
 
 export let states: any = {
-    skysynthVersion: "0.19.7",
-    skysynthLastUpdateDate: "2025-05-03",
+    skysynthVersion: "0.19.8",
+    skysynthLastUpdateDate: "2025-05-10",
     skysynthSheetVersion: "1.1",
         // sheet big version change: breaks
         // sheet small patch change: non-breaking
